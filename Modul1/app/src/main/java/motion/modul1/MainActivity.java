0829726fb1e2a0f2f55683b14eaec074cd5847a9
Muchamad Aqiel Hilman Maulandany <maqiel2600@gmail.com>
2018-09-28 21:14:44 +0700

    Fix Code

--- Modul1/app/src/main/java/motion/modul1/MainActivity.java
+++ Modul1/app/src/main/java/motion/modul1/MainActivity.java
@@ -3,20 +3,24 @@ package motion.modul1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
+    private TextView mTvNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTvNum = findViewById(R.id.tvNum);
     }
 
     public void onCountUp(View v){
         mCount++;
+        mTvNum.setText(String.valueOf(mCount));
     }
 
     public void onShowUp(View v){
