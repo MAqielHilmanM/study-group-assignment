7d7c55bde73b380c66d0e0ff3d126098de81a080
Muchamad Aqiel Hilman Maulandany <maqiel2600@gmail.com>
2018-09-28 20:38:34 +0700

    First Commit

--- Modul1/app/src/main/java/motion/modul1/MainActivity.java
+++ Modul1/app/src/main/java/motion/modul1/MainActivity.java
@@ -2,12 +2,24 @@ package motion.modul1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCountUp(View v){
+        mCount++;
+    }
+
+    public void onShowUp(View v){
+        Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT).show();
     }
 }
